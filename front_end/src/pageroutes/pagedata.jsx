ae0e36e9c5c12f2d75e2e266f93ca9e0ffef4331
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-04-20 11:34:39 -0700

    Added logout page to routes

--- front_end/src/pageroutes/pagedata.jsx
+++ front_end/src/pageroutes/pagedata.jsx
@@ -2,11 +2,12 @@ import LandingPage from "@/devcreatedcomponents/pages/landingpage";
 import LoginPage from "@/devcreatedcomponents/pages/login";
 import SignUpPage from "@/devcreatedcomponents/pages/signup";
 import Dashboard from "@/devcreatedcomponents/pages/dashboard";
+import LogOut from "@/devcreatedcomponents/pages/logout";
 const pageData = [
   {
     path: "",
     element: <LandingPage></LandingPage>,
-    title: "Landing Page",
+    title: "Home Page",
   },
   {
     path: "/signup",
@@ -16,13 +17,19 @@ const pageData = [
   {
     path: "/login",
     element: <LoginPage></LoginPage>,
-    title: "Log In",
+    title: "Log In Page",
   },
   {
     path: "/dashboard",
     element: <Dashboard></Dashboard>,
     title: "Dashboard",
   },
+
+  {
+    path: "/logout",
+    element: <LogOut></LogOut>,
+    title: "Log Out Page"
+  }
 ];
 
 export default pageData;
