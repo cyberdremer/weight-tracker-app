f6a870101cf8d1ddb3fc6133767f898bebf3566b
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-04-20 11:34:02 -0700

    Created skeleton for dashboard page

--- front_end/src/devcreatedcomponents/pages/dashboard.jsx
+++ front_end/src/devcreatedcomponents/pages/dashboard.jsx
@@ -1,6 +1,21 @@
-
+import { useState } from "react";
+import { Flex } from "@chakra-ui/react";
+import DashboardHeader from "../fragments/dashboardheader";
+import Drawer from "../fragments/drawer";
+import Menu from "../fragments/menu";
+import DrawerComponent from "../fragments/drawer";
 const Dashboard = () => {
-    
+  
+
+  return (
+    <>
+      <Flex grow="1" direction="column" minH="100vh">
+        <DashboardHeader
+        ></DashboardHeader>
+        
+      </Flex>
+    </>
+  );
 };
 
 export default Dashboard;
