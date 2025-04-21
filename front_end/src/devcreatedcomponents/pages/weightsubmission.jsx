caa09591a1ed2db378969fafd819f457b44e8fcf
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-04-20 19:56:24 -0700

    Added a weight entry form

---
+++ front_end/src/devcreatedcomponents/pages/weightsubmission.jsx
@@ -0,0 +1,15 @@
+import DashboardHeader from "../fragments/dashboardheader";
+import WeightForm from "../fragments/forms/weight";
+import { Flex } from "@chakra-ui/react";
+const WeightSubmission = () => {
+  return (
+    <>
+      <Flex grow="1" direction="column" minHeight="100vh">
+        <DashboardHeader></DashboardHeader>
+        <WeightForm></WeightForm>
+      </Flex>
+    </>
+  );
+};
+
+export default WeightSubmission;
