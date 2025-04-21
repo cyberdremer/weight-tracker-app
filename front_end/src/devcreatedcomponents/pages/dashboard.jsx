ba8694c627fc39a0be3c03e1248e0e1d29594787
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-04-20 19:54:51 -0700

    Added Dashboard charts to the dashboard page

--- front_end/src/devcreatedcomponents/pages/dashboard.jsx
+++ front_end/src/devcreatedcomponents/pages/dashboard.jsx
@@ -1,18 +1,15 @@
 import { useState } from "react";
 import { Flex } from "@chakra-ui/react";
 import DashboardHeader from "../fragments/dashboardheader";
-import Drawer from "../fragments/drawer";
-import Menu from "../fragments/menu";
-import DrawerComponent from "../fragments/drawer";
+
+import DashboardCharts from "../fragments/dashboardcharts";
 const Dashboard = () => {
-  
 
   return (
     <>
       <Flex grow="1" direction="column" minH="100vh">
-        <DashboardHeader
-        ></DashboardHeader>
-        
+        <DashboardHeader></DashboardHeader>
+        <DashboardCharts></DashboardCharts>
       </Flex>
     </>
   );
