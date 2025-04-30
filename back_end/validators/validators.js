c692f26313fa24ae854d2beeb4a0ab034a64d74d
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-04-30 10:58:24 -0700

    Updated login validators to not handle account existence or password correctness

---
+++ back_end/validators/validators.js
@@ -0,0 +1,83 @@
+const { body } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const prisma = require("../config/prismaclient");
+const emptyError = "this field cannot be empty!";
+const emailError = "Not a valid email address, please try again!";
+require("dotenv").config();
+const signupValidation = [
+  body("firstname")
+    .trim()
+    .notEmpty()
+    .withMessage(`First Name ${emptyError}`)
+    .escape(),
+  body("lastname")
+    .trim()
+    .notEmpty()
+    .withMessage(`Last Name: ${emptyError}`)
+    .escape(),
+
+  body("email")
+    .trim()
+    .notEmpty()
+    .withMessage(`Email: ${emptyError}`)
+    .normalizeEmail()
+    .isEmail()
+    .withMessage(`Email: ${emailError}`)
+    .custom(async (value) => {
+      const email = await prisma.user.findFirst({
+        where: {
+          email: value,
+        },
+      });
+      if (email) {
+        throw new Error(
+          "Email is already registered! Please use another email!"
+        );
+      }
+    }),
+
+  body("password")
+    .trim()
+    .notEmpty()
+    .withMessage(`Password :${emptyError}`)
+    .escape()
+    .isLength({ min: 8 })
+    .withMessage("Password must be a minimum of 8 characters long")
+    .matches(/^[a-zA-Z0-9!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]+$/)
+    .withMessage(
+      "Password must contain only alphanumeric and special characters"
+    ),
+
+  body("confirmpassword")
+    .trim()
+    .notEmpty()
+    .withMessage(`Confirm Password: ${emptyError}`)
+    .escape()
+    .custom(async (value, { req }) => {
+      return value === req.body.password;
+    })
+    .withMessage("Passwords must match!"),
+
+  body("signupcode")
+    .trim()
+    .notEmpty()
+    .withMessage(`Sign Up Code: ${emptyError}`)
+    .custom((value) => {
+      return value === process.env.SIGN_UP_CODE;
+    })
+    .withMessage(`This is not the correct sign up code! Please try again!`),
+];
+
+const loginValidation = [
+  body("email")
+    .trim()
+    .notEmpty()
+    .withMessage(`Email : ${emptyError}`)
+    .normalizeEmail()
+    .isEmail()
+    .withMessage(`This is not a valid email! Please enter a valid email!`),
+
+  body("password").trim().notEmpty().withMessage(`Password: ${emptyError}`),
+];
+
+module.exports = { signupValidation, loginValidation };
