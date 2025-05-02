5dede684899b216c71a39ff3dc4fe47dc372dc00
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-05-02 15:17:31 -0700

    Created success, error and delete alert

---
+++ front_end/src/devcreatedcomponents/alerts/alert.jsx
@@ -0,0 +1,73 @@
+import {
+  Button,
+  Dialog,
+  Portal,
+  CloseButton,
+  Input,
+  Alert,
+  Text,
+  Stack
+} from "@chakra-ui/react";
+import GenericModal from "../modal/genericmodal";
+
+const DeleteAccountAlert = ({ open, handleDelete, handleClose }) => {
+  return (
+    <GenericModal
+      open={open}
+      role="alertdialog"
+      title="Deleting Account"
+      handleClose={handleClose}
+      handleDelete={handleDelete}
+    >
+      <Stack gap="5">
+          <Text >
+            Are you sure you would like to delete your account? Enter your password
+            below to confirm account deletion
+          </Text>
+          <Input
+            type="password"
+            name="password"
+            placeholder="Enter your password"
+          ></Input>
+      </Stack>
+    </GenericModal>
+  );
+};
+
+const SuccessAlert = ({ message }) => {
+  return (
+    <Alert.Root
+      status="success"
+      variant="subtle"
+      animationName="slide-from-top"
+      animationDuration="slowest"
+      maxW="30%"
+      alignSelf="center"
+      position={"absolute"}
+      marginTop={2}
+    >
+      <Alert.Indicator />
+      <Alert.Title>{message}</Alert.Title>
+    </Alert.Root>
+  );
+};
+
+const ErrorAlert = ({ message }) => {
+  return (
+    <Alert.Root
+      status="error"
+      variant="subtle"
+      animationName="slide-from-top"
+      animationDuration="slowest"
+      maxW="30%"
+      alignSelf="center"
+      position={"absolute"}
+      marginTop={2}
+    >
+      <Alert.Indicator />
+      <Alert.Title>{message}</Alert.Title>
+    </Alert.Root>
+  );
+};
+
+export { ErrorAlert, SuccessAlert, DeleteAccountAlert };
