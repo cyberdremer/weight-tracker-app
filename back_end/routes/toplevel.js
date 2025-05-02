d894222e7f10bfc181edaee5b4f2f0fd38022ec1
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-05-02 15:49:17 -0700

    Added weight and account routes

--- back_end/routes/toplevel.js
+++ back_end/routes/toplevel.js
@@ -1,9 +1,13 @@
 const { Router } = require("express");
 const signupRouter = require("./signup");
 const loginRouter = require("./login");
+const weightRouter = require("./weight");
+const accountRouter = require("./account");
 
 const toplevelRouter = Router();
 toplevelRouter.use("/signup", signupRouter);
 toplevelRouter.use("/login", loginRouter);
+toplevelRouter.use("/account", accountRouter);
+toplevelRouter.use("/weight", weightRouter);
 
 module.exports = toplevelRouter;
