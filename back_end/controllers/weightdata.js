b3bea32b2c0280e40a863a18ffc805cd5199717f
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-05-02 22:02:12 -0700

    Added checks for invalid ranges

--- back_end/controllers/weightdata.js
+++ back_end/controllers/weightdata.js
@@ -1,9 +1,12 @@
 const asyncHandler = require("express-async-handler");
 const prisma = require("../config/prismaclient");
+const ensureAuthenticated = require("../middleware/authenticated");
+const ErrorWithStatusCode = require("../errors/statuscode");
 
 const getWeightDataForMonth = [
+  ensureAuthenticated,
   asyncHandler(async (req, res, next) => {
-    const searchedDate = new Date(req.params.date) || new Date();
+    const searchedDate = new Date();
     const firstDay = new Date(
       searchedDate.getFullYear(),
       searchedDate.getMonth(),
@@ -14,11 +17,11 @@ const getWeightDataForMonth = [
       searchedDate.getMonth() + 1,
       0
     );
-
+    const userId = req.session.passport.user;
     const dateData = await prisma.weightEntries.findMany({
       where: {
-        ownerid: req.user.id,
-        date: {
+        ownerid: userId,
+        createdat: {
           gte: firstDay,
           lte: lastDay,
         },
@@ -35,32 +38,54 @@ const getWeightDataForMonth = [
 ];
 
 const getWeightDataForSpecifiedTimeFrame = [
+  ensureAuthenticated,
   asyncHandler(async (req, res, next) => {
-    const { startDate, endDate } = req.params;
-    const firstDay = new Date(startDate.getFullYear(), startDate.getMonth(), 1);
-    const lastDay = new Date(
-      startDate.getFullYear(),
-      startDate.getMonth() + 1,
-      0
+    const startDate = new Date(
+      +req.params.startyear,
+      +req.params.startmonth - 1,
+      +req.params.startday
     );
+    const endDate = new Date(
+      +req.params.endyear,
+      +req.params.endmonth - 1,
+      +req.params.endday
+    );
+
+    if (startDate > endDate) {
+      throw new ErrorWithStatusCode(
+        "The start date can't be later than the end date!",
+        400
+      );
+    }
+
+    const userId = req.session.passport.user;
 
     const weightData = await prisma.weightEntries.findMany({
       where: {
-        ownerid: req.user.id,
-        date: {
-          gte: firstDay,
-          lte: lastDay,
+        ownerid: userId,
+        createdat: {
+          gte: startDate,
+          lte: endDate,
         },
       },
     });
 
-    res.status(302).json({
-      data: {
-        dates: weightData,
-        status: 302,
-      },
-    });
+    if (weightData.length === 0) {
+      res.status(404).json({
+        error: {
+          message: "There were no entries found",
+          statu: 404,
+        },
+      });
+    } else {
+      res.status(302).json({
+        data: {
+          dates: weightData,
+          status: 302,
+        },
+      });
+    }
   }),
 ];
 
-module.exports = {getWeightDataForMonth, getWeightDataForSpecifiedTimeFrame};
+module.exports = { getWeightDataForMonth, getWeightDataForSpecifiedTimeFrame };
