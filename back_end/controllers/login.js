fe1023669ec239636a8763e589bf40b25aebe4b0
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-05-03 15:49:21 -0700

    Updated succesfull response to contain message and cookie at the same time

--- back_end/controllers/login.js
+++ back_end/controllers/login.js
@@ -4,6 +4,7 @@ const { loginValidation } = require("../validators/validators");
 const asyncHandler = require("express-async-handler");
 const passport = require("passport");
 const ErrorWithStatusCode = require("../errors/statuscode");
+const e = require("express");
 
 const loginController = [
   loginValidation,
@@ -13,14 +14,25 @@ const loginController = [
       throw new ErrorWithStatusCode(errors.array()[0].msg, 400);
     }
 
-    passport.authenticate("local", { session: true })(req, res, next);
-
-    // res.status(200).json({
-    //   data: {
-    //     message: `${req.body.email} has logged in!`,
-    //     status: 200,
-    //   },
-    // });
+    passport.authenticate("local", { session: true }, (err, user, info) => {
+      if (err) {
+        return next(new ErrorWithStatusCode(err.msg, 500));
+      }
+      if (!user) {
+        return next(new ErrorWithStatusCode("Invalid credenitals", 400));
+      }
+      req.login(user, (err) => {
+        if (err) {
+          return next(err);
+        }
+        res.status(200).json({
+          data: {
+            message: "Login Succesfull!",
+            status: 200,
+          },
+        });
+      });
+    })(req, res, next);
   }),
 ];
 
