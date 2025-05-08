23bcb722ca961d00a056864f30266e6585cdba00
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-05-07 19:04:20 -0700

    Added routes for querying specific timeframes

--- back_end/routes/weight.js
+++ back_end/routes/weight.js
@@ -1,13 +1,19 @@
 const { Router } = require("express");
 const weightRouter = Router();
 const {
-  getWeightData,
+  getWeightDataForMonth,
   getWeightDataForSpecifiedTimeFrame,
 } = require("../controllers/weightdata");
-const postWeightEntry = require("../controllers/weightentry");
+const {
+  postWeightEntry,
+  deleteWeightEntry,
+} = require("../controllers/weightentry");
 
-weightRouter.get("/retrieve/:month?", getWeightData);
-weightRouter.get("/retrieve/:from-:to", getWeightDataForSpecifiedTimeFrame);
-weightRouter.post("/create", postWeightEntry);
-
+weightRouter.post("/entry", postWeightEntry);
+weightRouter.get(
+  "/retrieve/:startmonth-:startday-:startyear-:endmonth-:endday-:endyear",
+  getWeightDataForSpecifiedTimeFrame
+);
+weightRouter.get("/retrieve", getWeightDataForMonth);
+weightRouter.delete("/entry/:month-:day-:year", deleteWeightEntry);
 module.exports = weightRouter;
