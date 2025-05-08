50d1d1edd91a1cfc6fd58a005969a30bd1d5003f
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-05-07 18:56:56 -0700

    Created openai config

--- back_end/config/openaiconfig.js
+++ back_end/config/openaiconfig.js
@@ -1,6 +1,7 @@
-const OpenAI = require("openai");
+const {OpenAI} = require("openai");
+require("dotenv").config()
+
 const client = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
-
 module.exports = client;
