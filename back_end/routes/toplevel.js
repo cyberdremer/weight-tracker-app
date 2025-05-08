55105e3133df9c2e20274401259da7c54a002ea1
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-05-07 19:02:15 -0700

    Added final routes

--- back_end/routes/toplevel.js
+++ back_end/routes/toplevel.js
@@ -3,11 +3,15 @@ const signupRouter = require("./signup");
 const loginRouter = require("./login");
 const weightRouter = require("./weight");
 const accountRouter = require("./account");
+const logoutRouter = require("./logout");
+const dieticianRouter = require("./dietician");
 
 const toplevelRouter = Router();
 toplevelRouter.use("/signup", signupRouter);
 toplevelRouter.use("/login", loginRouter);
 toplevelRouter.use("/account", accountRouter);
 toplevelRouter.use("/weight", weightRouter);
+toplevelRouter.use("/logout", logoutRouter);
+toplevelRouter.use("/dietician", dieticianRouter);
 
 module.exports = toplevelRouter;
