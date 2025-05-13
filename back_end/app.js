21df84bf08050de057ec7a4c7af3b205a8715a51
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-05-13 16:00:26 -0700

    Added cors options and session logger middleware

--- back_end/app.js
+++ back_end/app.js
@@ -6,10 +6,22 @@ const passport = require("./config/passportconfig");
 const errorHandler = require("./middleware/errorhandler");
 const toplevelRouter = require("./routes/toplevel");
 const app = express();
+const sessionLogger = require("./middleware/sessionlogging");
+
+app.use(
+  cors({
+    credentials: true,
+    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+    exposedHeaders: ["set-cookie"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  })
+);
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(express.json());
 app.use(expressionSession);
+app.use(passport.initialize());
 app.use(passport.session());
+app.use(sessionLogger);
 app.use(toplevelRouter);
 
 app.use(errorHandler);
