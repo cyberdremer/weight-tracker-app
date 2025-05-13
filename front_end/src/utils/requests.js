31e2765d5326e00c9467c6c961d895ed8d03bbf2
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-05-13 16:01:50 -0700

    Created wrapper functions for network requests

---
+++ front_end/src/utils/requests.js
@@ -0,0 +1,125 @@
+import { data } from "react-router";
+import backendUrl from "./backendurl";
+const objectToForm = (form) => {
+  const formData = new FormData();
+  Object.entries(form).forEach(([key, value]) => {
+    formData.append(key, value);
+  });
+  return formData;
+};
+
+const postRequest = async (endpoint, body) => {
+  const bodyData = objectToForm(body);
+  const query = new URLSearchParams(bodyData).toString();
+  const response = await fetch(`${backendUrl + endpoint}`, {
+    method: "post",
+    mode: "cors",
+    body: query,
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  });
+  const data = await response.json();
+  if (!response.ok) {
+    throw new Error(data.error.message || response.statusText);
+  }
+
+  return data;
+};
+
+const protectedPostRequestJSON = async (endpoint, body) => {
+  const response = await fetch(`${backendUrl + endpoint}`, {
+    method: "post",
+    body: JSON.stringify(body),
+    credentials: "include",
+    mode: "cors",
+    headers: {
+      "content-type": "application/json",
+      accept: "application/json",
+      
+    },
+  });
+
+  const data = await response.json();
+  if (!response.ok) {
+    throw new Error(data.error.message || response.statusText);
+  }
+
+  return data;
+};
+const protectedPostRequest = async (endpoint, body) => {
+  const bodyData = objectToForm(body);
+  const query = new URLSearchParams(bodyData).toString();
+  const response = await fetch(`${backendUrl + endpoint}`, {
+    credentials: "include",
+    method: "post",
+    body: query,
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  });
+
+  const data = await response.json();
+  if (!response.ok) {
+    throw new Error(data.error.message || response.statusText);
+  }
+  return data;
+};
+
+const protectedGetRequest = async (endpoint) => {
+  const response = await fetch(`${backendUrl + endpoint}`, {
+    method: "get",
+    credentials: "include",
+    mode: "cors"
+  });
+  const data = await response.json();
+  if (!response.ok) {
+    throw new Error(data.error.message || response.statusText);
+  }
+  return data;
+};
+
+const protectedPutRequest = async (endpoint, body) => {
+  const bodyData = objectToForm(body);
+  const query = new URLSearchParams(bodyData).toString();
+  const response = await fetch(`${backendUrl + endpoint}`, {
+    method: "put",
+    body: query,
+    mode: "cors",
+    credentials: "include",
+    headers: {
+      "content-type": "application/x-www-form-urlencoded",
+    },
+  });
+
+  const data = await response.json();
+  if (!response.ok) {
+    throw new Error(data.error.message || response.statusText);
+  }
+  return data;
+};
+
+const protectedDeleteRequest = async (endpoint) => {
+  const response = await fetch(`${backendUrl + endpoint}`, {
+    method: "delete",
+    credentials: "include",
+    mode: "cors",
+  });
+
+  const data = await response.json();
+  if (!response.ok) {
+    throw new Error(data.error.message || response.statusText);
+  }
+  return data;
+};
+
+export {
+  protectedGetRequest,
+  protectedDeleteRequest,
+  protectedPostRequest,
+  protectedPutRequest,
+  postRequest,
+  protectedPostRequestJSON
+};
