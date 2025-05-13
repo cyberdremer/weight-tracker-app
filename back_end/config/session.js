3ff8339cc53ceb15974c0c0211e180812afb752a
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-05-13 15:04:15 -0700

    Updated session config for local testing

--- back_end/config/session.js
+++ back_end/config/session.js
@@ -1,24 +1,23 @@
 const prisma = require("./prismaclient");
 const { PrismaSessionStore } = require("@quixo3/prisma-session-store");
-const expressionSession = require("express-session");
+const session = require("express-session");
 require("dotenv").config();
 
-const sessionConfig = expressionSession({
-  cookie: {
-    maxAge: 7 * 24 * 60 * 60 * 1000,
-    path: "/",
-    secure: false
-    // sameSite:"none",
-    // httpOnly: true,
-    // secure: false,
-  },
+const sessionConfig = session({
   secret: process.env.SESSION_SECRET,
-  resave: true,
+  cookie: {
+    maxAge: 24 * 60 * 60 * 1000,
+    secure: false,
+    sameSite: "lax"
+  },
+
+  resave: false,
   saveUninitialized: false,
   store: new PrismaSessionStore(prisma, {
     checkPeriod: 2 * 60 * 1000, //ms
-    dbRecordIdIsSessionId: true,
+    dbRecordIdIsSessionId: false,
     dbRecordIdFunction: undefined,
+    logger: console 
   }),
 });
 
