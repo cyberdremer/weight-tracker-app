ec636e653c1fb4eb52147bc649823b920b002331
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-05-13 17:48:20 -0700

    Added form handling to dashboard component

--- front_end/src/devcreatedcomponents/pages/dashboard.jsx
+++ front_end/src/devcreatedcomponents/pages/dashboard.jsx
@@ -1,15 +1,139 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { Flex } from "@chakra-ui/react";
+import ViewEntries from "../fragments/viewentries";
 import DashboardHeader from "../fragments/dashboard/dashboardheader";
-
+import useFetchData from "../effects/hooks";
 import DashboardCharts from "../fragments/dashboard/dashboardcharts";
+import { protectedGetRequest } from "@/utils/requests";
+import {
+  calculateBMIUsingImperial,
+  calulateBMIUsingMetric,
+} from "@/utils/bmicalculator";
+import { InfoContext } from "../context/InfoContext";
+
+const mockItems = [
+  {
+    id: 1,
+    weight: 70,
+    date: "2023-10-01",
+    notes: "Feeling good",
+  },
+  {
+    id: 2,
+    weight: 72,
+    date: "2023-10-02",
+    notes: "A bit tired",
+  },
+  {
+    id: 3,
+    weight: 71,
+    date: "2023-10-03",
+    notes: "Had a good workout",
+  },
+  {
+    id: 4,
+    weight: 69,
+    date: "2023-10-04",
+    notes: "Feeling great",
+  },
+  {
+    id: 5,
+    weight: 68,
+    date: "2023-10-05",
+    notes: "Need to eat more protein",
+  },
+];
 const Dashboard = () => {
+  // const { entries, error, loading, setEntries } =
+  //   useFetchData("/weight/retrieve");
+  const { user } = useContext(InfoContext);
+  const setEntries = () => {};
+
+  const [isImperial, setIsImperial] = useState(true);
+
+  const [form, setForm] = useState({
+    startdate: "",
+    enddate: "",
+  });
+
+  const [responseError, setResponseError] = useState({
+    error: false,
+    message: "",
+  });
+
+  const [responseSuccess, setResponseSuccess] = useState({
+    success: false,
+    message: "",
+  });
+
+  const handleSearchEntries = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await protectedGetRequest(`/weight/retrieve`, form);
+      if (!response.error) {
+        throw new Error(response.error.message);
+      }
+      setResponseSuccess({
+        success: true,
+        message: response.data.message,
+      });
+
+      setTimeout(() => {
+        setResponseSuccess({
+          success: false,
+          message: "",
+        });
+      }, timer);
+    } catch (error) {
+      setResponseError({
+        error: true,
+        message: error.message,
+      });
+
+      setTimeout(() => {
+        setResponseError({
+          error: false,
+          message: "",
+        });
+      }, timer);
+    }
+  };
+
+  const handleRefreshEntries = async (e) => {
+    e.preventDefault();
+    try {
+    } catch (error) {
+      const response = await protectedGetRequest(`/weight/retrieve`);
+      if (!response.error) {
+        throw new Error(response.error.message);
+      }
+      setResponseSuccess({
+        success: true,
+        message: response.data.message,
+      });
+      setEntries(response.data.entries);
+      setTimeout(() => {
+        setResponseSuccess({
+          success: false,
+          message: "",
+        });
+      }, timer);
+    }
+  };
 
   return (
     <>
-      <Flex grow="1" direction="column" minH="100vh">
-        <DashboardHeader></DashboardHeader>
-        <DashboardCharts></DashboardCharts>
+      <Flex grow="1" direction="column" minH="100vh" gap="10  ">
+        <DashboardHeader name={user.fullname}></DashboardHeader>
+        <DashboardCharts
+          entries={mockItems}
+          setEntries={setEntries}
+          handleRefreshEntries={handleRefreshEntries}
+          handleSearchEntries={handleSearchEntries}
+          form={form}
+          setForm={setForm}
+        ></DashboardCharts>
+        <ViewEntries entries={mockItems}></ViewEntries>
       </Flex>
     </>
   );
