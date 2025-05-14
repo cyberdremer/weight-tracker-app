452db54f0ac4c2eb2e84c4a34af68ebb1b7bd866
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-05-13 17:49:42 -0700

    Added error and success popups to weight submission

--- front_end/src/devcreatedcomponents/pages/weightsubmission.jsx
+++ front_end/src/devcreatedcomponents/pages/weightsubmission.jsx
@@ -1,12 +1,81 @@
 import DashboardHeader from "../fragments/dashboard/dashboardheader";
 import WeightForm from "../fragments/forms/weight";
 import { Flex } from "@chakra-ui/react";
+import { useState } from "react";
+import { SuccessAlert, ErrorAlert } from "../alerts/alert";
+import { protectedPostRequest } from "@/utils/requests";
+const env = import.meta.env.VITE_ENV;
+const prodTimer = import.meta.env.VITE_PROD_ALERT_TIMER;
+const devTimer = import.meta.env.VITE_DEV_ALERT_TIMER;
+const timer = env === "dev" ? devTimer : prodTimer;
 const WeightSubmission = () => {
+  const [form, setForm] = useState({
+    weight: "",
+    date: "",
+    notes: "",
+  });
+
+  const handleTextChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+  const [responseError, setResponseError] = useState({
+    error: false,
+    message: "",
+  });
+
+  const [responseSuccess, setResponseSuccess] = useState({
+    success: false,
+    message: "",
+  });
+
+  const handleEntrySubmission = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await protectedPostRequest("/weight/entry", form);
+      if (response.error) {
+        throw new Error(response.error.message);
+      }
+      setResponseSuccess({
+        success: true,
+        message: response.data.message,
+      });
+
+      setTimeout(() => {
+        setResponseSuccess({
+          success: false,
+          message: "",
+        });
+      }, timer);
+    } catch (error) {
+      setResponseError({
+        error: true,
+        message: error.message,
+      });
+
+      setTimeout(() => {
+        setResponseError({
+          error: false,
+          message: "",
+        });
+      }, timer);
+    }
+  };
+
   return (
     <>
       <Flex grow="1" direction="column" minHeight="100vh">
+        {responseError.error && (
+          <ErrorAlert message={responseError.message}></ErrorAlert>
+        )}
+        {responseSuccess.success && (
+          <SuccessAlert message={responseSuccess.message}></SuccessAlert>
+        )}
         <DashboardHeader></DashboardHeader>
-        <WeightForm></WeightForm>
+        <WeightForm
+          handleEntrySubmission={handleEntrySubmission}
+          handleChange={handleTextChange}
+          form={form}
+        ></WeightForm>
       </Flex>
     </>
   );
