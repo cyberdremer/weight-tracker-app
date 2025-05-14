abd9fc950679420671af4f57c65aca7302aedfa9
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-05-13 17:00:46 -0700

    Finalized main app structure

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -5,16 +5,18 @@ import { Button, Container, HStack, Flex } from "@chakra-ui/react";
 import LandingPage from "./devcreatedcomponents/pages/landingpage";
 import SignUp from "./devcreatedcomponents/pages/signup";
 import LoginPage from "./devcreatedcomponents/pages/login";
-
+import { InfoProvider } from "./devcreatedcomponents/context/InfoContext";
 import { BrowserRouter } from "react-router";
 import PageRoutes from "./pageroutes/pageroutes";
 
 function App() {
   return (
     <Container minHeight="100vh" maxWidth="100%" padding="0" margin="0">
-      <BrowserRouter>
-        <PageRoutes></PageRoutes>
-      </BrowserRouter>
+      <InfoProvider>
+        <BrowserRouter>
+          <PageRoutes></PageRoutes>
+        </BrowserRouter>
+      </InfoProvider>
     </Container>
   );
 }
--- front_end/src/main.jsx
+++ front_end/src/main.jsx
@@ -1,17 +1,23 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "./components/ui/provider.jsx";
+import { AuthProvider } from "./devcreatedcomponents/context/AuthContext.jsx";
+import { InfoProvider } from "./devcreatedcomponents/context/InfoContext.jsx";
 import App from "./App.jsx";
-import "@fontsource-variable/inter/index.css"
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import "@fontsource-variable/inter/index.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider>
-      <App />
+      <AuthProvider>
+       
+          <App />
+        
+      </AuthProvider>
     </Provider>
   </StrictMode>
 );
