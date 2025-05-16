2c19914601c0a0efdb57d0803299edb27fe9b2cf
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-05-16 16:50:16 -0700

    Updated delete account controller to delete other relations

--- back_end/controllers/account.js
+++ back_end/controllers/account.js
@@ -1,26 +1,59 @@
 const asyncHandler = require("express-async-handler");
 const { validationResult } = require("express-validator");
-const { updateAccountValidation } = require("../validators/validators");
+const {
+  updateAccountValidation,
+  deleteAccountValidation,
+} = require("../validators/validators");
 const prisma = require("../config/prismaclient");
 const ErrorWithStatusCode = require("../errors/statuscode");
 const ensureAuthenticated = require("../middleware/authenticated");
+const bcryptjs = require("bcryptjs");
 
 const deleteAccount = [
   ensureAuthenticated,
+  deleteAccountValidation,
   asyncHandler(async (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       throw new ErrorWithStatusCode(errors.array()[0].msg, 400);
     }
-    const deletedAccount = await prisma.user.delete({
+
+    const accountToDelete = await prisma.user.findFirstOrThrow({
       where: {
-        id: req.session.passport.user,
+        id: req.user.id,
       },
     });
 
-    if (!deletedAccount) {
+    const passwordMatch = await bcryptjs.compare(
+      req.body.password,
+      accountToDelete.passwordhash
+    );
+    if (!passwordMatch) {
+      throw new ErrorWithStatusCode("Incorrect Credentials!", 404);
+    }
+
+    if (!accountToDelete) {
       throw new ErrorWithStatusCode("Account not found!", 404);
     }
+
+    const deletionPromise = Promise.all([
+      await prisma.dietEntries.deleteMany({
+        where: {
+          ownerid: req.user.id,
+        },
+      }),
+      await prisma.weightEntries.deleteMany({
+        where: {
+          ownerid: req.user.id,
+        },
+      }),
+    ]);
+
+    const deletedAccount = await prisma.user.delete({
+      where: {
+        id: req.user.id,
+      },
+    });
 
     res.status(200).json({
       data: {
@@ -39,13 +72,15 @@ const updateAccount = [
     if (!errors.isEmpty()) {
       throw new ErrorWithStatusCode(errors.array()[0].msg, 400);
     }
+
+    const height =
+      req.user.isImperial === true ? req.body.height * 2.5 : req.body.height ;
     await prisma.user.update({
       where: {
-        id: req.session.passport.user,
+        id: req.user.id,
       },
       data: {
-        dob: req.body.dob,
-        isImperial: req.body.units === "lbs" ? true : false,
+        height: height,
       },
     });
 
