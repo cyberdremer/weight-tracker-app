c819caea3403956f5db75ac35d6b6e52f2165bb6
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-05-16 17:59:04 -0700

    Created file to programatically change the timer for alerts and popups

---
+++ front_end/src/utils/timer.js
@@ -0,0 +1,7 @@
+const env = import.meta.env.VITE_ENV;
+const prodTimer = import.meta.env.VITE_PROD_ALERT_TIMER;
+const devTimer = import.meta.env.VITE_DEV_ALERT_TIMER;
+const timer = env === "dev" ? devTimer : prodTimer;
+
+
+export default timer;
