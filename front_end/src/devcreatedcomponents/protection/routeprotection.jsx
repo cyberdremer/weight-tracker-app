d4624b75c398cf0118b4e73e970567f786cff09f
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-05-19 16:38:13 -0700

    Added server check to see if session is still valid

--- front_end/src/devcreatedcomponents/protection/routeprotection.jsx
+++ front_end/src/devcreatedcomponents/protection/routeprotection.jsx
@@ -1,10 +1,24 @@
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
+import { Flex } from "@chakra-ui/react";
 import { useNavigate } from "react-router";
+import LoadingPlaceholder from "../fragments/loading";
 import { Navigate } from "react-router";
 const RequireAuth = ({ children }) => {
-  const { authed } = useContext(AuthContext);
+  const { authed, loading, error } = useContext(AuthContext);
   const navigate = useNavigate();
+  if (loading) {
+    return (
+      <>
+        <Flex grow="1" direction="column" minH="100vh">
+          <LoadingPlaceholder></LoadingPlaceholder>
+        </Flex>
+      </>
+    );
+  }
+  if (error) {
+    <Navigate to="/login"></Navigate>;
+  }
   return authed ? children : <Navigate to="/login"></Navigate>;
 };
 
