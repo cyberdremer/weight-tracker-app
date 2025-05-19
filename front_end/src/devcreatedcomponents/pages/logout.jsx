a2ee928fb8359060b2c27680bc13d2d492833f91
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-05-19 16:42:56 -0700

    Added hook to destory session

--- front_end/src/devcreatedcomponents/pages/logout.jsx
+++ front_end/src/devcreatedcomponents/pages/logout.jsx
@@ -1,7 +1,16 @@
+import { useContext, useEffect } from "react";
 import Header from "../fragments/header";
 import LogOutBlurb from "../fragments/logoutblurb";
 import { Flex } from "@chakra-ui/react";
+import { AuthContext } from "../context/AuthContext";
 const LogOut = () => {
+  const { destroySession } = useContext(AuthContext);
+  useEffect(() => {
+    const logOutAndRedirect = async () => {
+      await destroySession();
+    };
+    logOutAndRedirect();
+  }, []);
   return (
     <Flex grow="1" direction="column" minHeight="100vh">
       <Header></Header>
