c01032297cdc92e94af97aa326694053dc647229
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-05-19 16:25:28 -0700

    Added origin url to list of origins

--- back_end/app.js
+++ back_end/app.js
@@ -11,7 +11,11 @@ const sessionLogger = require("./middleware/sessionlogging");
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+    origin: [
+      "http://localhost:5173",
+      "http://127.0.0.1:5173",
+      process.env.ORIGIN_URL,
+    ],
     exposedHeaders: ["set-cookie"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   })
