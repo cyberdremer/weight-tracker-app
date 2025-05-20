88b1663b502ac16f2ceb242a79d83089b0a53aaf
David Lara <73867161+cyberdremer@users.noreply.github.com>
2025-05-19 19:40:11 -0700

    Added email field to user validation

--- front_end/src/devcreatedcomponents/context/AuthContext.jsx
+++ front_end/src/devcreatedcomponents/context/AuthContext.jsx
@@ -1,30 +1,62 @@
 import { createContext, useContext, useEffect, useState } from "react";
+import { protectedGetRequest, logoutRequest } from "@/utils/requests";
+import { InfoContext } from "./InfoContext";
 
 const AuthContext = createContext();
+
 const AuthProvider = ({ children }) => {
-  const [authed, setAuthed] = useState(() => {
-    const storedAuth = localStorage.getItem("authed");
-    return storedAuth === "true";
-  });
+  const { setUser } = useContext(InfoContext);
+  const [authed, setAuthed] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
   // TODO rewrite login and logout to make calls to the server. authenticate and logout routes respectively
 
-  const logOut = () => {
-    setAuthed(false);
-    localStorage.setItem("authed", false);
-  };
+  
 
-  const logIn = () => {
-    setAuthed(true);
-    localStorage.setItem("authed", true);
+  const destroySession = async () => {
+    try {
+      const response = await logoutRequest();
+      if (response.error) {
+        throw new Error(response.error.message);
+      }
+      setAuthed(false);
+      
+    } catch (error) {}
   };
 
   useEffect(() => {
-    const storedAuth = localStorage.getItem("authed");
-    storedAuth === "true" ? setAuthed(true) : setAuthed(false);
-  });
+    const verifySession = async () => {
+      try {
+        const response = await protectedGetRequest("/authenticate");
+        if (response.error) {
+          throw new Error(response.error.message);
+        }
+        const { fullname, height, id, isImperial, email, dateofbirth } = response.data.user;
+        setUser({
+          fullname: fullname,
+          height: height,
+          id: id,
+          isImperial: isImperial,
+          email: email,
+          dateofbirth: dateofbirth
+        });
+        setAuthed(true);
+      } catch (error) {
+        setAuthed(false);
+        setUser(null);
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    verifySession();
+  }, []);
 
   return (
-    <AuthContext.Provider value={{ authed, logIn, logOut }}>
+    <AuthContext.Provider
+      value={{ authed, destroySession, loading, error, setAuthed }}
+    >
       {children}
     </AuthContext.Provider>
   );
